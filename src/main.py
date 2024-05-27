a082f826291205cee3c231ea7055cfef73a00dc0
PabloPazosRego <84968042+PabloPazosRego@users.noreply.github.com>
2024-05-27 18:43:50 +0200

    Incorporacion logging por print

--- src/main.py
+++ src/main.py
@@ -2,22 +2,24 @@ import sqlite3  # Para SQLite
 from nats.aio.client import Client as NATS
 import random, argparse, time
 import asyncio, serial
-
-# Creacion Base de Datos
+import logging
 
 class Database:
+    ''' 
+        Gestion de la base de datos: creación y subida de datos
+    '''
+
     def __init__(self, db_url) -> None:
         self.db_url = db_url
-        #self.db_conn = sqlite3.connect(db_url)
         self.create_connection()
         self.create_table()
 
     def create_connection(self):
         try:
             self.db_conn = sqlite3.connect(self.db_url)
-            print('Realizada conexion con exito')
+            logging.info(f'DB:: Successful connection.')
         except Exception as e:
-            print(f'Imposible conectar con la base de datos: {e}')
+            logging.error(f'DB:: Unable to connect to the database: {e}')
             
 
     def create_table(self):
@@ -35,11 +37,15 @@ class Database:
             print(str(data).encode())
             cursor.execute("INSERT INTO sensor_data (value) VALUES (?)", (str(data),))
             self.db_conn.commit()
+            logging.info(f'DB:: Data stored correctly.')
         except sqlite3.Error as e:
-            print(f'Error subiendo datos al DB {e}')
+            logging.error(f"DB:: Error storing data in database: {e}")
     
-# Creacion del Sensor
 class Sensor:
+    '''
+        Objeto del sensor, pudiendo generar diferentes sensores. 
+        Se gestiona si la cogida de datos es artificial o de un sensor real.
+    '''
     def __init__(self, sensor_type, serial_port =None, baud_rate = None):
         self.sensor_type = sensor_type
         self.serial_port = serial_port
@@ -52,7 +58,8 @@ class Sensor:
         try:
             self.serial_conn = serial.Serial(self.serial_port, self.baud_rate)
         except Exception as e:
-            print(f'Error en la conexion al sensor: {e}')
+             logging.error(f"SENSOR:: Error opening serial port: {e}")
+
     
     def read_sensor_data(self):
         ''' Leer datos del sensor '''
@@ -61,9 +68,10 @@ class Sensor:
         else:
             try:
                 data = self.serial_conn.readline().decode().strip()
+                logging.info(f'SENSOR:: Data collected from real sensor: {data}')
                 return data
             except Exception as e:
-                print(f'Error leyendo del sensor: {e}')
+                logging.error(f"SENSOR:: Error reading from serial port: {e}")
 
 class DataPublisher:
     '''
@@ -76,23 +84,21 @@ class DataPublisher:
     async def connect_to_nats(self):
         try:
             await self.nats_client.connect(servers=["nats://localhost:4222"], connect_timeout=10)
-
-            #await self.nats_client.connect('nats://localhost:4222')
         except Exception as e:
-            print(f'Imposible conectarse al servidor NATS {e}')
+            logging.error(f"NATS:: Error connecting to NATS server: {e}")
     
     async def publish_data(self, data):
         try:
             await self.nats_client.publish('sensor.data', str(data).encode())
         except Exception as e:
-            print(f'Imposible publidar datos en el servidor NATS {e}')
-
+            logging.error(f"NATS:: Error publishing data to NATS server: {e}")
 
 '''
     Inicializacion del programa
     Se añaden los argumentos solicitados para introducir cada vez que se ejecute la APP se puedan modificar en la llamada.
 '''
 async def main():
+    
     parser = argparse.ArgumentParser(description="Sensor Application")
     
     parser.add_argument("--sensor_type", choices=["mockup", "real"], default="mockup", help="Tipo de sensor: real o mockup")
@@ -101,6 +107,8 @@ async def main():
     parser.add_argument("--frequency", type=int, default=5, help="Frecuencia de lectura del sensor en segundos.")
     parser.add_argument("--db_uri", default="../sensor_data.db", help="URL de conexion a la base de datos.")
 
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    
     args = parser.parse_args()
 
     sensor = Sensor(args.sensor_type, args.serial_port, args.baud_rate)
@@ -114,8 +122,8 @@ async def main():
         if data:
             await data_publisher.publish_data(data)
             database.store_data(data)
-        #time.sleep(args.frequency) #Paralizar la solicitud de datos el tiempo marcado como frecuencia
-        await asyncio.sleep(args.frequency)
+        await asyncio.sleep(args.frequency) #Paralizar la solicitud de datos el tiempo marcado como frecuencia
+
 
 if __name__ == "__main__":
     asyncio.run(main())
